141c6c7b0d028ba80bbaf84797f02f1412434775
Harry Sun <harrysun2006@gmail.com>
2020-08-26 12:40:14 +1000

    update

--- src/main/java/com/momenton/orgchart/OrgChart.java
+++ src/main/java/com/momenton/orgchart/OrgChart.java
@@ -9,11 +9,10 @@ import com.momenton.orgchart.exception.MultipleCEOException;
 import com.momenton.orgchart.exception.OrgChartException;
 
 import java.io.BufferedReader;
-import java.io.File;
 import java.io.FileReader;
+import java.io.InputStreamReader;
 import java.nio.file.Paths;
 import java.util.ArrayList;
-import java.util.Arrays;
 import java.util.Comparator;
 import java.util.HashMap;
 import java.util.HashSet;
@@ -137,13 +136,12 @@ public class OrgChart {
     }
 
     public static void main(String[] args) throws Exception {
-        File input;
+        BufferedReader br;
         if (args.length == 0) {
-            input = new File(ClassLoader.getSystemResource("input.txt").toURI());
+            br = new BufferedReader(new InputStreamReader(ClassLoader.getSystemResourceAsStream("input.txt")));
         } else {
-            input = Paths.get(args[0]).toFile();
+            br = new BufferedReader(new FileReader(Paths.get(args[0]).toFile()));
         }
-        BufferedReader br = new BufferedReader(new FileReader(input));
         String line;
         List<String> data = new ArrayList<>();
         while ((line = br.readLine()) != null) {
